a965378f00e398875692d848c97bbf6bb4191d24
LEEDAESUN <deasun.lee@ctr.co.kr>
2026-07-21 14:14:57 +0900

    누락된 데이터·로고 파일 추가 (patrol_data.js, gear_data.js, ctr_logo.png)

---
+++ gear_data.js
@@ -0,0 +1,10 @@
+window.GEAR_DATA = {"rows":[
+ {"no":1,"grp":"안전모","name":"다목적용 안전모","mgmt":"","cert":null},
+ {"no":2,"grp":"안전대","name":"벨트 하네스","mgmt":"HS-8334","cert":null},
+ {"no":3,"grp":"안전화","name":"발보호 안전화 (A)","mgmt":"","cert":null},
+ {"no":4,"grp":"안전화","name":"정전기 방지 안전화","mgmt":"","cert":null},
+ {"no":5,"grp":"안전화","name":"구두형 안전화","mgmt":"","cert":null},
+ {"no":6,"grp":"보안경","name":"고글형 보안경","mgmt":"10-AV1GM-0251","cert":null},
+ {"no":7,"grp":"방열복","name":"방열복","mgmt":"","cert":null},
+ {"no":8,"grp":"방열복","name":"방열보호 장갑","mgmt":"19-AV4CS-0003","cert":null}
+]};
